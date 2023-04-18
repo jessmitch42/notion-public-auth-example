cd40d495d95969312b4bd11ad93434ff98df8482
Jess Mitchell <jess@jessmitchell.ca>
2023-04-18 13:32:44 -0400

    add button to test

--- src/lib/notion.js
+++ src/lib/notion.js
@@ -1,3 +1,6 @@
+import { Client } from "@notionhq/client";
+let notion;
+
 const getOAuthAccessToken = async (code) => {
   try {
     const response = await fetch("/api/notion", {
@@ -11,7 +14,25 @@ const getOAuthAccessToken = async (code) => {
   }
 }
 
+const setToken = (token) => {
+  notion = new Client({ auth: token });
+}
+
+const getUsers = async (token) => {
+  // set token before making api request
+  if (!notion) {
+    setToken(token)
+  }
+
+  try {
+    const response = await notion.users.list();
+    return response.results;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export {
-  getOAuthAccessToken
+  getOAuthAccessToken,
+  getUsers
 }
